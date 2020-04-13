2355024e06344029da13c02cec5cae41c1b457f1
Jasmine <36653195+Jasmine-fe@users.noreply.github.com>
2020-04-13 10:26:55 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,9 @@ import "firebase/auth";
 import "firebase/database";
 import "firebase/storage";
 
+// 需要替換成自己 firebase 的資訊
 var firebaseConfig = {
-  apiKey: "AIzaSyDSkHpCGDF5h-LT4CVxQmCGJvmIOLL6AhY",
-  authDomain: "react-slack-clone-c0095.firebaseapp.com",
-  databaseURL: "https://react-slack-clone-c0095.firebaseio.com",
-  projectId: "react-slack-clone-c0095",
-  storageBucket: "react-slack-clone-c0095.appspot.com",
-  messagingSenderId: "880329047744",
-  appId: "1:880329047744:web:b4095cef336a83cafa3725",
-  measurementId: "G-VVS537GJT1"
+  
 };
 
 firebase.initializeApp(firebaseConfig);
